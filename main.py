10cecd4e15d9f3b1b18d79b88881b28df26fafdb
Gautier Deleglise <gautier.deleglise@gmail.com>
2016-03-16 20:52:20 +0100

    Ajout des bateaux dans la matrice
    Création 2 fonctions au lieu d'une pour les lignes de window1 et
    window2.
    Ajout des bateaux a la matrice.
    Comment reload la page depuis la fonction après bateau ajouté a la
    matrice -> a voir

--- main.py
+++ main.py
@@ -19,15 +19,96 @@ import tkinter.filedialog
 #Toutes les fonctions
 #===========================
 
-#Detection souris
-def pointeur(event):
+#Detection souris J1
+def pointeurJ1(event):
+    global bateaux1
+    global bateau
     casex=ceil(float(0.02)*float(str(event.x)))
     casey=ceil(float(0.02)*float(str(event.y)))
+    print(casex)
+    print(casey)
     if casex>1 and casey>1:
-        print('Ok')
+#MODE HORIZONTAL
+        if mode=="h":
+            if bateau==1:
+                if casex<7:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-2,casex-1]==0 and bateaux1[casey-2,casex]==0 and bateaux1[casey-2,casex+1]==0 and bateaux1[casey-2,casex+2]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-2,casex-1]=3
+                        bateaux1[casey-2,casex]=3
+                        bateaux1[casey-2,casex+1]=3
+                        bateaux1[casey-2,casex+2]=4
+                        bateau=2
+            elif bateau==2:
+                if casex<8:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-2,casex-1]==0 and bateaux1[casey-2,casex]==0 and bateaux1[casey-2,casex+1]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-2,casex-1]=3
+                        bateaux1[casey-2,casex]=3
+                        bateaux1[casey-2,casex+1]=4
+                        bateau=3
+            elif bateau==3:
+                if casex<9:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-2,casex-1]==0 and bateaux1[casey-2,casex]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-2,casex-1]=3
+                        bateaux1[casey-2,casex]=4
+                        bateau=4
+            elif bateau==4:
+                if casex<9:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-2,casex-1]==0 and bateaux1[casey-2,casex]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-2,casex-1]=3
+                        bateaux1[casey-2,casex]=4
+                        bateau=5
+            elif bateau==5:
+                if casex<10:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-2,casex-1]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-2,casex-1]=4
+#MODE VERTICAL
+        elif mode=="v":
+            if bateau==1:
+                if casey<7:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-1,casex-2]==0 and bateaux1[casey,casex-2]==0 and bateaux1[casey+1,casex-2]==0 and bateaux1[casey+2,casex-2]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-1,casex-2]=3
+                        bateaux1[casey,casex-2]=3
+                        bateaux1[casey+1,casex-2]=3
+                        bateaux1[casey+2,casex-2]=4
+                        bateau=2
+            elif bateau==2:
+                if casey<8:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-1,casex-2]==0 and bateaux1[casey,casex-2]==0 and bateaux1[casey+1,casex-2]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-1,casex-2]=3
+                        bateaux1[casey,casex-2]=3
+                        bateaux1[casey+1,casex-2]=4
+                        bateau=3
+            elif bateau==3:
+                if casey<9:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-1,casex-2]==0 and bateaux1[casey,casex-2]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-1,casex-2]=3
+                        bateaux1[casey,casex-2]=4
+                        bateau=4
+            elif bateau==4:
+                if casey<9:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-1,casex-2]==0 and bateaux1[casey,casex-2]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-1,casex-2]=3
+                        bateaux1[casey,casex-2]=4
+                        bateau=5
+            elif bateau==5:
+                if casey<10:
+                    if bateaux1[casey-2,casex-2]==0 and bateaux1[casey-1,casex-2]==0:
+                        bateaux1[casey-2,casex-2]=2
+                        bateaux1[casey-1,casex-2]=4
+        print(bateaux1)
+
 #============================================================================
 #Creation lignes dans window 1 et window2
-def lignes():
+def lignes1():
     #Lignes horizontale fenetre 1
     window1.create_line(500, 50, 0, 50, width=2)
     window1.create_line(500, 100, 0, 100, width=2)
@@ -71,7 +152,7 @@ def lignes():
     window1.create_text(25,375,text='7')
     window1.create_text(25,425,text='8')
     window1.create_text(25,475,text='9')
-
+def ligne2():
 #Ligne horizontale fenetre 2
     window2.create_line(500, 50, 0, 50, width=2)
     window2.create_line(500, 100, 0, 100, width=2)
@@ -173,8 +254,6 @@ def gm3():
 #============================================================================
 #Changer gamemode -> 4
 def gm4():
-    global joueur
-    joueur=1
     BoutonJouer.destroy()
     BoutonImporter.destroy()
     BoutonRegles.destroy()
@@ -183,12 +262,32 @@ def gm4():
     window1.delete(ALL)
     window2.delete(ALL)
      #Fenetre de choix
-    Largeur = 500
-    Hauteur = 500
+    global joueur
+    try:
+        joueur
+    except NameError:
+        joueur=1
+    #bateau:
+    global bateau
+    try:
+        bateau
+    except NameError:
+        bateau=1
+
+    #mode:
+    #h= horizontal
+    #v= vertical
+    global mode
+    try:
+        mode
+    except NameError:
+        mode="h"
+
     img1 = window1.create_image(50,50,anchor=NW, image=photo)
-    lignes()
+    lignes1()
 
 #Creation matrice
+    global bateaux1
     bateaux1=zeros((9,9))
     bateaux1[8,6]=2
     bateaux1[8,7]=3
@@ -218,7 +317,7 @@ def gm4():
 
 #Detection clic souris
 
-    window1.bind("<Button-1>", pointeur)
+    window1.bind("<Button-1>", pointeurJ1)
 
 #==============================================================================
 #window1=Fenetre gauche de jeu
@@ -248,6 +347,13 @@ def gm4():
 #bateaux1=Matrice J1
 #bateaux2=Matrice J2/IA
 #============================
+#Bateaux:
+# 1- 5 cases
+# 2- 4 cases
+# 3- 3 cases
+# 4- 3 cases
+# 5- 2 cases
+#=============================================================================
 
 Mafenetre = Tk()
 Mafenetre.title('Bataille navale')
@@ -269,7 +375,7 @@ menubar.add_cascade(label="Aide", menu=menuaide)
 #affichage barre d'outil
 Mafenetre.config(menu=menubar)
 #=============================
-#Définition des canevas
+#Definition des canevas
 Largeur=500
 Hauteur=500
 window1 = Canvas(Mafenetre, width = Largeur, height =Hauteur, bg ='white')
