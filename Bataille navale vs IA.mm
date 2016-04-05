98c9e70aab83bf3e2b3998018f5b4b622687d53b
Gautier <gautier.deleglise@gmail.com>
2016-04-05 12:48:04 +0200

    Freeplane
    Mise en ligne du freeplane, à vous de le completer

---
+++ Bataille navale vs IA.mm
@@ -0,0 +1,377 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Bataille navale vs IA" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1453371696659"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="Pr&#xe9;paration" POSITION="right" ID="ID_1723987269" CREATED="1453371770738" MODIFIED="1453371953981" HGAP="120" VSHIFT="-10">
+<edge COLOR="#ff0000"/>
+<node TEXT="Identifier le besoin" ID="ID_1628210388" CREATED="1453371927900" MODIFIED="1453371984001">
+<node TEXT="Cr&#xe9;er un jeu de bataille navale" ID="ID_129341624" CREATED="1454580366335" MODIFIED="1454580415993"/>
+</node>
+<node TEXT="D&#xe9;finir le probl&#xe8;me &#xe0; r&#xe9;soudre" ID="ID_1985052931" CREATED="1453371936409" MODIFIED="1453372000641">
+<node TEXT="Comment jouer &#xe0; une bataille navale &#xe0; 2 joueurs sur 1 ordinateur" ID="ID_411831820" CREATED="1454580614945" MODIFIED="1454580707461"/>
+</node>
+<node TEXT="Plannification" ID="ID_1948020563" CREATED="1453371937250" MODIFIED="1453372006881">
+<node TEXT="Pr&#xe9;sentation des &#xe9;tapes du projet" ID="ID_61163187" CREATED="1453372028519" MODIFIED="1454577632957">
+<node TEXT="Gautier" ID="ID_1763172142" CREATED="1454577652853" MODIFIED="1454577751692">
+<cloud COLOR="#00ff00" SHAPE="ARC"/>
+<node TEXT="Intelligence artificelle" ID="ID_300290640" CREATED="1453373075108" MODIFIED="1454577778976">
+<node TEXT="Diff&#xe9;rentes difficult&#xe9;es" ID="ID_674789421" CREATED="1454577345496" MODIFIED="1454577353076"/>
+</node>
+<node TEXT="Sauvegarder les fichiers" ID="ID_1269451779" CREATED="1454579198557" MODIFIED="1454579210393"/>
+</node>
+<node TEXT="Paul" ID="ID_726394346" CREATED="1454577671355" MODIFIED="1454577734656">
+<cloud COLOR="#00ffff" SHAPE="ARC"/>
+<node TEXT="D&#xe9;roulement du jeu" ID="ID_912826369" CREATED="1453373060358" MODIFIED="1454577703987">
+<node TEXT="Ecran d&apos;attente a la fin de chaque tour" ID="ID_1500950538" CREATED="1453373232728" MODIFIED="1453373247530"/>
+<node TEXT="Ghost de croix lorsque l&apos;on veut cliquer" ID="ID_431375309" CREATED="1453373250058" MODIFIED="1453373364913"/>
+<node TEXT="Touch&#xe9;/coul&#xe9;" ID="ID_1441872413" CREATED="1453373411555" MODIFIED="1453373572657"/>
+</node>
+</node>
+<node TEXT="Alexandre" ID="ID_821960429" CREATED="1454577685189" MODIFIED="1454577771862">
+<cloud COLOR="#ffff00" SHAPE="ARC"/>
+<node TEXT="Cr&#xe9;ation de la fen&#xea;tre graphique" ID="ID_183291578" CREATED="1453372362777" MODIFIED="1454577617763"/>
+<node TEXT="Cr&#xe9;ation de l&apos;&#xe9;cran d&apos;accueil" ID="ID_1284162389" CREATED="1453372497046" MODIFIED="1454577617763"/>
+<node TEXT="Cr&#xe9;ation de l&apos;&#xe9;cran de jeu" ID="ID_345773124" CREATED="1453372776739" MODIFIED="1454577617763"/>
+<node TEXT="Placement des bateaux" ID="ID_939789118" CREATED="1453372806298" MODIFIED="1454577617763"/>
+</node>
+</node>
+<node TEXT="Calendrier pr&#xe9;visionnel" ID="ID_333885318" CREATED="1453372028919" MODIFIED="1453372047991"/>
+</node>
+<node TEXT="Evaluation" ID="ID_1134483690" CREATED="1453371937939" MODIFIED="1453372019771"/>
+</node>
+<node TEXT="Programme principal" POSITION="left" ID="ID_1417854784" CREATED="1453372264196" MODIFIED="1459853074747" HGAP="-360" VSHIFT="666">
+<edge COLOR="#00ff00"/>
+<node TEXT="Gautier" ID="ID_528934912" CREATED="1459851391697" MODIFIED="1459851400145" HGAP="-200" VSHIFT="-40">
+<node TEXT="PvP" ID="ID_1944672710" CREATED="1459851533774" MODIFIED="1459853124345" HGAP="-110" VSHIFT="-40">
+<node TEXT="PointeurJeuJ1+PointeurJeuJ2" ID="ID_1092650408" CREATED="1459851986652" MODIFIED="1459852040006" HGAP="-250" VSHIFT="-50"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cette fonction permet d'&#233;crire dans la matrice de son adversaire en fonction de son tir
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="gm1" ID="ID_1853369437" CREATED="1459852569857" MODIFIED="1459853079964" HGAP="-110" VSHIFT="-10"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D&#233;tection si fin de partie + affichage des bateaux du joueur correspondant
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="PvIA" ID="ID_757588369" CREATED="1459851550326" MODIFIED="1459853121394" HGAP="-110" VSHIFT="-10">
+<node TEXT="IA" ID="ID_1712736963" CREATED="1459852183693" MODIFIED="1459852223495" HGAP="-90" VSHIFT="-30"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pr&#233;paration du mode de jeu vs IA + g&#233;n&#233;ration al&#233;atoire de la matrice de l'IA
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="PointeurIAJ1" ID="ID_1573397527" CREATED="1459852230928" MODIFIED="1459852672825" HGAP="-150"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Prise en compte du tir du joueur + mise en place du tir de l'IA.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Facile: L'IA tire al&#233;atoirement
+    </p>
+    <p>
+      Moyen: L'IA traque un bateau jusqu'&#224; sa destruction
+    </p>
+    <p>
+      Difficile: L'IA traque un bateau jusqu'&#224; sa destruction(optimis&#233;) + r&#233;fl&#233;chit &#224; ses tirs pour les optimiser
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="gm2" ID="ID_617959642" CREATED="1459852666225" MODIFIED="1459853084443" HGAP="-100" VSHIFT="20"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D&#233;tection si fin de partie + affichage des bateaux du joueur
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Placement bateaux" ID="ID_145368826" CREATED="1459851761894" MODIFIED="1459851792908" HGAP="-190" VSHIFT="20">
+<node TEXT="Mode" ID="ID_1272322159" CREATED="1459851690772" MODIFIED="1459851837938" HGAP="-200" VSHIFT="-20"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cette fonction permet de changer le mode de placement des bateaux (Horizontal-&gt;Vertical,Vertical-&gt;Horizontal)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="AffichageBateaux" ID="ID_1306463819" CREATED="1459851588118" MODIFIED="1459851894536" HGAP="-270"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cette fonction permet d'afficher les bateaux des joueurs 1 et 2 lors de la phase de placement des bateaux
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="J2" ID="ID_741170951" CREATED="1459851913440" MODIFIED="1459853087284" HGAP="-180" VSHIFT="10"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cette fonction permet de passer au joueur 2 lors de la s&#233;lection des bateaux
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="PointeurJ1+PointeurJ2" ID="ID_1066502132" CREATED="1459852052492" MODIFIED="1459853093380" HGAP="-300" VSHIFT="10"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Choisir o&#249; positionner son bateau + v&#233;rification que cela est possible
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="gm4" ID="ID_281003171" CREATED="1459852772014" MODIFIED="1459852845206" HGAP="-190" VSHIFT="20"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Affichage du bateau a placer sur la droite+affichage bateaux
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Sauvegarder" ID="ID_818721196" CREATED="1459852400318" MODIFIED="1459853129847" HGAP="-160" VSHIFT="70">
+<node TEXT="PointeurSave" ID="ID_1177695628" CREATED="1459852408213" MODIFIED="1459853090776" HGAP="-190" VSHIFT="-20"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D&#233;tection clic souris dans une zone pour choisir le lieu de sauvegarde.
+    </p>
+    <p>
+      Enregistrement de la partie en cours (PvP only) dans une matrice sous la forme -&gt;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &lt;space&gt;
+    </p>
+    <p>
+      &lt;matrice J1&gt;
+    </p>
+    <p>
+      &lt;space&gt;
+    </p>
+    <p>
+      &lt;matrice J2&gt;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="gm3" ID="ID_974950804" CREATED="1459852707588" MODIFIED="1459852739000" HGAP="-140" VSHIFT="-10"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Display graphique+v&#233;rification si sauvegarde possible
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Autre" ID="ID_945972966" CREATED="1459852847947" MODIFIED="1459853136012" HGAP="-120" VSHIFT="70">
+<node TEXT="wait" ID="ID_1536693119" CREATED="1459852859111" MODIFIED="1459853096379" HGAP="-100" VSHIFT="-10"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Temps d'attente a afficher sur l'&#233;cran
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="time_is_up1+time_is_up2" ID="ID_1908528013" CREATED="1459852904130" MODIFIED="1459853099132" HGAP="-230"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Fonction a &#233;xecuter a la fin du temps (Aurait pu faire mieux)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="reset_affichage" ID="ID_447664074" CREATED="1459852964182" MODIFIED="1459853110015" HGAP="-170" VSHIFT="20"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reset tout ce qu'il y a dans window1 et window2, unbind et enl&#232;ve les boutons
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="reset" ID="ID_1911620988" CREATED="1459853018621" MODIFIED="1459853107587" HGAP="-110" VSHIFT="20"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reset total de toutes les variables pour se pr&#233;parer &#224; une nouvelle partie
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
